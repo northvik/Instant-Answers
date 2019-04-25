86b82b916d4be646f760b3ff06599bcd026c9c48
northvik <camillepire@northvik.fr>
2019-04-25 21:50:32 +0100

    Add regex cheat sheet

---
+++ app/src/modules/regex_cheat_sheet/public/javascript/script.js
@@ -0,0 +1,25 @@
+/**
+ * This is your main script file. Please refer to the documentation for more information.
+ */
+
+var IARuntime = function() {
+    function Regex_cheat_sheet (iaData) {
+        // constructor
+    }
+
+    /**
+     * runs at runtime
+     */
+    Regex_cheat_sheet.prototype.run = function() {
+        // function that's gonna run at runtime
+    };
+
+    /**
+     * runs upon exit
+     */
+    Regex_cheat_sheet.prototype.stop = function() {
+        // function that's gonna run upon exit
+    };
+
+    return Regex_cheat_sheet;
+}();
